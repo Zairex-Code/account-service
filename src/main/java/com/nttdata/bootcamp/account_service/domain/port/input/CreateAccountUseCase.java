21abbef79def9c8658678c8174bfcae211eae409
Zairex-Code <dylanyesidflorez@gmail.com>
2026-08-12 14:51:29 -0500

    feature(CreateAccountUseCase) created

---
+++ src/main/java/com/nttdata/bootcamp/account_service/domain/port/input/CreateAccountUseCase.java
@@ -0,0 +1,25 @@
+package com.nttdata.bootcamp.account_service.domain.port.input;
+
+import com.nttdata.bootcamp.account_service.domain.model.Account;
+import reactor.core.publisher.Mono;
+
+
+/**
+ * Input Port interface defining the contract for bank account creation workflows
+ *
+ * Technical & Business Rules:
+ * - Pure Java domain interface decoupled from Framework, MongoDB or WebFlux controllers
+ * - Serves as the primary entry point for evaluating and opening financial bank account
+ * - Enforces product holding limits per customer type (Personal vs Business)
+ * - Validate minimum opening balance requirements based on customer profile (STANDARD, VIP , PYME)
+ */
+public interface CreateAccountUseCase {
+
+    /**
+     * Evaluates domain business rules and creates a new bank account asynchronously
+     *
+     * @param account Domain entity instance containing the requested account parameters
+     * @return A Mono emitting the newly persisted Account domain entity.
+     */
+    Mono<Account> execute(Account account);
+}
